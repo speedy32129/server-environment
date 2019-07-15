497cb2c6757da56c70db5f69a366935259108894
Jim McAleer <ironspeeder@hotmail.com>
2019-07-14 21:52:46 -0400

    add database project

---
+++ DB-Server-Environment/dbo/Tables/UserAuditEvents.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserAuditEvents] (
+    [UserAuditId] INT                IDENTITY (1, 1) NOT NULL,
+    [AuditEvent]  INT                NOT NULL,
+    [IpAddress]   NVARCHAR (MAX)     NULL,
+    [Timestamp]   DATETIMEOFFSET (7) NOT NULL,
+    [UserId]      NVARCHAR (MAX)     NOT NULL,
+    CONSTRAINT [PK_UserAuditEvents] PRIMARY KEY CLUSTERED ([UserAuditId] ASC)
+);
+
